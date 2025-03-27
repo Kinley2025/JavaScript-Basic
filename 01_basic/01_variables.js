6dcede5c810a841c569a126c0498b109a41a28cc
Kinley2025 <kwangdi556@gmail.com>
2025-03-27 18:43:03 +0000

    01_variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,23 @@
+const emailId = "kinley@gmail.com"
+let accountId = 126789
+var passwordSaved = 1989
+countryName = "Bhutan"
+let accountSate;
+
+// emailId = "Karma@gmail.com" (Not allowed to change const)
+
+
+accountId = 2025
+passwordSaved = "Pangkang"
+countryName = "India"
+
+
+
+// prefer not to use var because issue in block scope and functional scope
+
+console.table([accountId, passwordSaved, countryName, accountSate])
+
+
+
+
+
