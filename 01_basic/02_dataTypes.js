73f7e1905c604902f7626be2eca9b8ea2420bb2c
Kinley2025 <kwangdi556@gmail.com>
2025-03-27 18:40:38 +0000

    variables and dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; 
+// treat all Js code as newer version
+
+// alert( 3 + 3 ) we are using Node.js not browser
+
+console.log(3 +
+    3); 
+    // code readability should be high
+
+
+    let name = "Kinley Wangdi"
+    let age = 36
+    let isLoggedIn = true
+
+    // Number => 2 to the power of 3
+    // String => ""
+    // boolean => true/false
+    // BigInt
+    // null => standalone value
+    // undefined =>
+    // symbol => unique
+
+
+    // Object
+
+
+    // console.log(typeof undefined); => undefined
+    // console.log(typeof null) => Object;
+    
+    
