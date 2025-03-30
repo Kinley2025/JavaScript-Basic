9ad56b756595b5c4f0fddd0df00e40b3a9f311c6
Kinley2025 <kwangdi556@gmail.com>
2025-03-30 09:21:08 +0000

    nums and math done

--- 01_basic/06_nums_and_math.js
+++ 01_basic/06_nums_and_math.js
@@ -1,8 +1,39 @@
-const score = 400;
-console.log(score);
+// const score = 400;
+// console.log(score);
  
 const number = new Number (400)
- console.log(number);
+//  console.log(number);
+
+// console.log(number.toString().length);
+
+// console.log(number.toFixed(8));
+
+// const newNumber = 222.60;
+// console.log(newNumber.toPrecision(3));
+
+// const oldNumber = 10000000;
+// console.log(oldNumber.toLocaleString("en-IN"));
+
+// ********************* Math **************************
+
+// console.log(Math);
+// // console.log(Math.abs(-200));
+// console.log(Math.round(99.7));
+
+// console.log(Math.random());
+// console.log(Math.random()*10 + 1);
+
+// const min = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
 
 
  
