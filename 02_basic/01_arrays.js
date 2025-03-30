a0436984ba72b3c9e39b16396996275acbee543d
Kinley2025 <kwangdi556@gmail.com>
2025-03-30 12:36:18 +0000

    basic Arrays done

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,37 @@
+// ********** Arrays **********
+
+// const myArrays = [1, 2, 3, 4, 5];
+//  console.log(myArrays[0]);
+
+//  ==========> Array Methods <=========
+
+// myArrays.push("Kinley");
+// console.log(myArrays);
+
+// myArrays.pop();
+// console.log(myArrays);
+
+// myArrays.unshift(null);
+// console.log(myArrays);
+
+// myArrays.shift();
+// console.log(myArrays);
+
+const myArr = [1, 2, 3, 4, 5, 6];
+// console.log(newArr.includes("a"));
+// console.log(newArr.indexOf(3));
+
+const newArrays = myArr.join();
+// console.log(myArr);
+// console.log(typeof newArrays);
+console.log(myArr.slice(1,3));
+
+
+
+
+
+
+
+
+
+ 
