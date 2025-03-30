340ff084b644bfb85dad36b6d13458943db2e064
Kinley2025 <kwangdi556@gmail.com>
2025-03-30 13:30:04 +0000

    array done

---
+++ 02_basic/02_array.js
@@ -0,0 +1,32 @@
+// const vegItems = ["potatos", "tomatos", "pumkin"];
+// const nonVegItems = ["beef", "pork", "chicken"]
+//    vegItems.push(nonVegItems);
+//     console.log(vegItems);
+
+// const allItems = vegItems.concat(nonVegItems);
+//   console.log(allItems);
+
+// const allNewItems = [...vegItems, ...nonVegItems];
+//   console.log(allNewItems);
+  
+// let arryNum1 = [1, 2, 3, 4, 5, 8, [9, 10, [11, 12]]];
+// let arryNum2 = arryNum1.flat(Infinity);
+// console.log(arryNum2);
+
+// console.log(Array.isArray("Kinley"));
+// console.log(Array.from("Kinley"));
+
+// console.log(Array.from ({fullName : "Kinley"})); ====> output it gives empty Array []
+
+// const age1 = 19;
+// const age2 = 30;
+// const age3 = 40;
+//          console.log(Array.of(age1, age2, age3));
+         
+
+
+
+
+  
+    
+   
