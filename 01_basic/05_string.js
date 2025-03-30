751e7540ffea523337a3a2f8bdee215c39d52811
Kinley2025 <kwangdi556@gmail.com>
2025-03-30 07:48:53 +0000

    string done

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -1,12 +1,12 @@
-const myName = "Kinley Wangdi";
-const myAge = 36;
+// const myName = "Kinley Wangdi";
+// const myAge = 36;
 
 // console.log(myName + myAge); dont code like this its outdated.
 
-console.log(`my name is ${myName} I'am ${myAge} years old`)
+// console.log(`my name is ${myName} I'am ${myAge} years old`)
 
 
-const yourName = new String ("Pema Wangmo")
+// const yourName = new String ("Pema Wangmo")
 
 // Method of String
 // console.log(yourName.length);
@@ -17,6 +17,50 @@ const yourName = new String ("Pema Wangmo")
 
 //  console.log(`index of ${index} return character ${yourName.at(index)}`);
 
+// 2- charAt
+
+// const herName = "Sonam Wangmo";
+
+// let index = 6;
+// console.log(`index of ${index} return character of ${herName.charAt(index)}`);
+
+// 3- charCodeAt
+
+// const newString = "I love to dancing and sing";
+// let index = 12;
+// console.log(`index of ${index} return character ${newString.charCodeAt(index)}`);
+
+// 4- codePointAt
+
+// const icon = "🎉⛔🚗";
+// console.log(icon.codePointAt(2));
+
+// 5- concat
+
+// const firstName = "Kinley";
+// const secondName = " Wangdi";
+//    fullName = firstName + secondName
+
+//       console.log(fullName);
+
+
+// 6- endsWith()
+
+// const dogsBark = "All the dogs bark"
+// console.log(dogsBark.endsWith("dogs"));
+// console.log(dogsBark.endsWith("k"));
+
+
+// 7- includes()
+
+// const wordIncluded = "You known that were Karma is from?"
+// const word = "jadha";
+// console.log(`The words ${word} ${wordIncluded.includes(word)? "is" : "is not"} in the sentence`,);
+
+
+
+
+      
 
 
 
