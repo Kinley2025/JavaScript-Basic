18c7547ffd363c61388ca2fda14e561604ecda95
Kinley2025 <kwangdi556@gmail.com>
2025-03-30 12:35:36 +0000

    data done

---
+++ 01_basic/07_datesinJS.js
@@ -0,0 +1,18 @@
+// *************** Date ***************
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+
+// console.log(typeof myDate); =====> output its gives Object.
+
+// let newCreatedDate = new Date(2025, 0, 24);
+// console.log(newCreatedDate.toString());
+// console.log(newCreatedDate.toDateString());
+
+
+
+
